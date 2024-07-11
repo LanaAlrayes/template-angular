c2df72a1703b4b0a6c9eb1741be25753a9b40604
LanaAlrayes <LanaAlrayss@gmail.com>
2024-07-11 12:39:15 +0300

    add Student

--- src/app/views/admin/addstudents/addstudents/addstudents.component.ts
+++ src/app/views/admin/addstudents/addstudents/addstudents.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from 'src/app/views/services/data.service';
 
 @Component({
   selector: 'app-addstudents',
@@ -7,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class AddstudentsComponent {
 
+  messageErr=""
+  constructor(private ds: DataService, private router: Router) { }
+  add(f: any) {
+    let data = f.value
+    console.log(data)
+
+    this.ds.Addstudents(data).subscribe(response => {
+      // console.log(response)
+      this.router.navigate(['/admin/allstudents'])
+    }, (err: HttpErrorResponse) => {
+      this.messageErr=err.error
+
+      // console.log(err.error) // text error
+      // console.log(err.status) // 400
+    })
+
+  }
 }
