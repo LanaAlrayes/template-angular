9de92ced27033432bd9899f26d6eaefcbdef0b5e
LanaAlrayes <LanaAlrayss@gmail.com>
2024-07-15 14:34:14 +0300

    add component studentdetails

---
+++ src/app/views/admin/studentdetails/studentdetails/studentdetails.component.ts
@@ -0,0 +1,23 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from 'src/app/views/services/data.service';
+
+@Component({
+  selector: 'app-studentdetails',
+  templateUrl: './studentdetails.component.html',
+  styleUrls: ['./studentdetails.component.css']
+})
+export class StudentdetailsComponent {
+  id=''
+  dataObject:any
+  messageErr=''
+  constructor(private route:ActivatedRoute , private ds:DataService ){
+    // this.route.params.subscribe(params=> this.id=params.id)
+
+    this.ds.getOnestudent(this.id).subscribe(res=>this.dataObject=res
+      ,(err:HttpErrorResponse)=>
+      this.messageErr='error')
+  }
+
+}
