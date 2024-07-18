f2f36afc8861d665042878915aa072412f310b6a
LanaAlrayes <LanaAlrayss@gmail.com>
2024-07-18 11:04:43 +0300

    Admin Layout some changes in environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+    production: false,
+    secret: 'winopapa',
+    client: '123456789',
+    urlBackend: 'https://jsonplaceholder.typicode.com/'
+};
