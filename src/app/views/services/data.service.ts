1146c40117b1c668cb09b628f9f6906024663fd8
LanaAlrayes <LanaAlrayss@gmail.com>
2024-07-18 10:37:16 +0300

    add admin layout protect

--- src/app/views/services/data.service.ts
+++ src/app/views/services/data.service.ts
@@ -1,32 +1,42 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  token: any = localStorage.getItem('token')
 
+  headerAdmin = new HttpHeaders()
+    .set('authorization', this.token)
+    .set('role', 'admin ')
 
+  params = new HttpParams()
+    .set('secret', 'wiopapa')
+    .set('client', '12456987')
+
+  headerall = new HttpHeaders()
+    .set('authorization', this.token)
 
   constructor(private http: HttpClient) { }
 
   getAllstudents() {
-    return this.http.get('https://jsonplaceholder.typicode.com/users')
+    return this.http.get('https://jsonplaceholder.typicode.com/users', { headers: this.headerall, params: this.params })
   }
 
   Addstudents(profile: any) {
-    return this.http.post('https://jsonplaceholder.typicode.com/users', profile)
+    return this.http.post('https://jsonplaceholder.typicode.com/users', profile, { headers: this.headerAdmin, params: this.params })
   }
 
   deletestudent(id: any) {
-    return this.http.delete('https://jsonplaceholder.typicode.com/users' + id)
+    return this.http.delete('https://jsonplaceholder.typicode.com/users' + id, { headers: this.headerAdmin, params: this.params })
   }
 
   updateStudent(id: string, newProfile: any) {
-    return this.http.patch('https://jsonplaceholder.typicode.com/users' + id, newProfile)
+    return this.http.patch('https://jsonplaceholder.typicode.com/users' + id, newProfile, { headers: this.headerAdmin, params: this.params })
   }
 
-  getOnestudent(id:any){
-    return this.http.get('https://jsonplaceholder.typicode.com/users' + id)
+  getOnestudent(id: any) {
+    return this.http.get('https://jsonplaceholder.typicode.com/users' + id, { headers: this.headerall, params: this.params })
   }
 }
