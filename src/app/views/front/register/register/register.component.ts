320ac03114f610fd7e2f45671663ed76a081ed53
LanaAlrayes <LanaAlrayss@gmail.com>
2024-07-23 14:24:02 +0300

    message error

--- src/app/views/front/register/register/register.component.ts
+++ src/app/views/front/register/register/register.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthuserService } from 'src/app/views/services/authuser.service';
@@ -8,15 +9,20 @@ import { AuthuserService } from 'src/app/views/services/authuser.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  messageError: any
+
   constructor(private aus: AuthuserService, private router: Router) { }
 
 
   register(f: any) {
     let data = f.value
-    
+
     this.aus.register(data).subscribe(data => {
       this.router.navigate(['/login'])
       console.log(data)
+    }, (err: HttpErrorResponse) => {
+      console.log('message error')
+      this.messageError = "Error"
     })
   }
 }
