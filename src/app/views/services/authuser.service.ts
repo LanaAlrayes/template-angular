d7961fdb5676bca63193c032c66446d6c788f16d
LanaAlrayes <LanaAlrayss@gmail.com>
2024-07-28 10:02:11 +0300

    loggedIn

--- src/app/views/services/authuser.service.ts
+++ src/app/views/services/authuser.service.ts
@@ -1,10 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthuserService {
+
+  helper = new JwtHelperService();
 
   constructor(private http: HttpClient) { }
 
@@ -15,4 +18,26 @@ export class AuthuserService {
   login(body: any) {
     return this.http.post('https://jsonplaceholder.typicode.com', body)
   }
+
+  saveToken(token: any) {
+    localStorage.setItem('tokenUser', token)
+  }
+
+  userLoggedIn() {
+    let token: any = localStorage.getItem('token')
+    if (!token) {
+      return false
+    }
+
+    let decodeToken = this.helper.decodeToken(token)
+    if (decodeToken.role) {
+      return false
+    }
+
+    if (this.helper.isTokenExpired(token)) {
+      return false
+    }
+    return true
+  }
 }
+
