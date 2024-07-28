87b630d470a82896b2ec44f513cba8b2963ac97d
LanaAlrayes <LanaAlrayss@gmail.com>
2024-07-28 10:01:38 +0300

    login user

--- src/app/views/front/loginuser/loginuser/loginuser.component.ts
+++ src/app/views/front/loginuser/loginuser/loginuser.component.ts
@@ -1,4 +1,6 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { AuthuserService } from 'src/app/views/services/authuser.service';
 
 @Component({
   selector: 'app-loginuser',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./loginuser.component.css']
 })
 export class LoginuserComponent {
+  messageError: any
+  datatoken: any
+  constructor(private asd: AuthuserService) { }
 
+  login(f: any) {
+    let data = f.value
+    this.asd.login(data).subscribe(data => {
+      this.datatoken = data
+      this.asd.saveToken(this.datatoken.token)
+    }, (err: HttpErrorResponse) => this.messageError=err.error.error)
+  }
 }
